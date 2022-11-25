5af6b54e04e30599505cf2c8e4d1d070783b76d5
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-11-25 17:07:35 +0600

    Basic layout and router setup

---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,300 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+// import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
+
+const Header = () => {
+  const [mdOptionsToggle, setMdOptionsToggle] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
+
+  // const { user, logOut } = useContext(AuthContext);
+
+  // const handleLogOut = () => {
+  //   logOut()
+  //     .then()
+  //     .catch((error) => console.error("Logout Error: ", error));
+  // };
+
+  return (
+    <div className="dark:bg-gray-900">
+      <div>
+        <div className="relative">
+          {/* For large screens */}
+          <div className="dark:bg-gray-900 bg-gray-50 px-6 py-9">
+            <div className="container mx-auto flex items-center justify-between">
+              <Link
+                to="/"
+                className="md:w-2/12 cursor-pointer text-gray-800 dark:text-white font-bold text-2xl"
+                aria-label="Home made."
+              >
+                book<span className="text-indigo-500">Sell</span>
+              </Link>
+              <ul className="hidden w-8/12 md:flex items-center justify-center space-x-8">
+                <li>
+                  <Link
+                    to="/"
+                    className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/blog"
+                    className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                {/* {user?.email ? (
+                  <>
+                    <li>
+                      <Link
+                        to="/myreviews"
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        My Reviews
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/addservice"
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        Add Service
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        onClick={handleLogOut}
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        Log Out
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <li>
+                    <Link
+                      to="/login"
+                      className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                )} */}
+              </ul>
+              <div className="md:w-2/12 justify-end flex items-center space-x-4 xl:space-x-8">
+                <div className="flex lg:hidden">
+                  <button
+                    aria-label="show options"
+                    onClick={() => setMdOptionsToggle(!mdOptionsToggle)}
+                    className="text-black dark:text-white dark:hover:text-gray-300 hidden md:flex focus:outline-none focus:ring-2 rounded focus:ring-gray-600"
+                  >
+                    <svg
+                      className="fill-stroke"
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 6H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M10 12H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M6 18H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    aria-label="open menu"
+                    onClick={() => setShowMenu(true)}
+                    className="text-black dark:text-white dark:hover:text-gray-300 md:hidden focus:outline-none focus:ring-2 rounded focus:ring-gray-600"
+                  >
+                    <svg
+                      className="fill-stroke"
+                      width={24}
+                      height={24}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 6H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M10 12H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M6 18H20"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* For small screen */}
+          <div
+            id="mobile-menu"
+            className={`${
+              showMenu ? "flex" : "hidden"
+            } absolute dark:bg-gray-900 z-10 inset-0 md:hidden bg-white flex-col h-screen w-full`}
+          >
+            <div className="flex items-center justify-between border-b border-gray-200 dark:border-gray-700 pb-4 p-4">
+              <button
+                onClick={() => setShowMenu(false)}
+                aria-label="close menu"
+                className="focus:outline-none focus:ring-2 rounded focus:ring-gray-600 ml-auto"
+              >
+                <svg
+                  className="fill-stroke text-gray-800 dark:text-white"
+                  width={16}
+                  height={16}
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 4L4 12"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M4 4L12 12"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-6 p-4">
+              <ul className="flex flex-col space-y-6">
+                <li>
+                  <a
+                    href="/"
+                    className="dark:text-white flex items-center justify-between hover:underline text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800"
+                  >
+                    Home
+                    <div>
+                      <svg
+                        className="fill-stroke text-black dark:text-white"
+                        width={12}
+                        height={12}
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4.5 3L7.5 6L4.5 9"
+                          stroke="currentColor"
+                          strokeWidth="0.75"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <Link
+                    to="/blog"
+                    className="dark:text-white flex items-center justify-between hover:underline text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800"
+                  >
+                    Blog
+                    <div>
+                      <svg
+                        className="fill-stroke text-black dark:text-white"
+                        width={12}
+                        height={12}
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4.5 3L7.5 6L4.5 9"
+                          stroke="currentColor"
+                          strokeWidth="0.75"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </Link>
+                </li>
+                {/* {user?.email ? (
+                  <>
+                    <li>
+                      <Link
+                        to="/myreviews"
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        My Reviews
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/addservice"
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        Add Service
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        onClick={handleLogOut}
+                        className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                      >
+                        Log Out
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <li>
+                    <Link
+                      to="/login"
+                      className="dark:text-white text-base text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                )} */}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
